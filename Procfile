5c98647debf3dee2d5d6118acff18e9c2e0db0f4
pgr301-exam <pgr301exam@gmail.com>
2018-11-20 23:39:59 +0100

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -jar target/herokupipe-example-0.0.1-SNAPSHOT.jar 
---
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -0,0 +1,36 @@
+package com.example.herokupipeexample;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+      this.customerRepository = customerRepository;
+    }
+
+    @RequestMapping("/")
+    public String welcome() {
+        return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
+    }
+
+    @RequestMapping("/list")
+    public List<Customer> find(@RequestParam(value="lastName") String lastName) {
+        return customerRepository.findByLastName(lastName);
+    }
+
+    @PostMapping("/")
+    	Customer newCustomer(@RequestBody Customer customer) {
+        System.out.println(customer);
+    		return customerRepository.save(customer);
+    	}
+
+}
